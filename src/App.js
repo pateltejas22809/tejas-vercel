0bcd2041f3cb5c4ba104cd92d4a5b7250e369f7e
tejas <pateltejas2809@gmail.com>
2023-08-24 11:30:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      OM SAI RAM
+        
+      
+    </div>
+  );
+}
+
+export default App;
